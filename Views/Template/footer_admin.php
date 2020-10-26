fb9708afd7175ad0874c04f7149e04dc625bb58b
Grijalba Osorio <juan.grijalba@alcaldiacali.local>
2020-10-26 13:33:34 -0500

    act

--- Views/Template/footer_admin.php
+++ Views/Template/footer_admin.php
@@ -16,6 +16,5 @@
     <script type="text/javascript" src="<?= media(); ?>/js/plugins/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="<?= media(); ?>/js/plugins/dataTables.bootstrap.min.js"></script>
     <script src="<?= media(); ?>/js/functions_roles.js"></script>
-    </script>
   </body>
 </html>
