a0d1684f022d4e260d0a1278a4446ca3675f5daa
JuanDa97 <juandavidgo1997@gmail.com>
2020-10-25 22:18:22 -0500

    act 1

--- Views/Template/Modals/modalRoles.php
+++ Views/Template/Modals/modalRoles.php
@@ -10,30 +10,30 @@
       </div>
       <div class="modal-body">
         <div class="tile">
-            <div class="tile-body">
-                <form id="formRol" name="formRol">
-                <input type="hidden" id="idRol" name="idRol" value=""> 
-                <div class="form-group">
-                  <label class="control-label">Nombre</label>
-                  <input class="form-control" id="txtNombre" name="txtNombre" type="text" placeholder="Nombre del Rol" required="">
-                </div>
-                <div class="form-group">
-                  <label class="control-label">Descripción</label>
-                  <textarea class="form-control" id="txtDescripcion" name="txtDescripcion" rows="2" placeholder="Descripcion del Rol" required=""></textarea>
-                </div>
-                <div class="form-group">
-                    <label for="exampleSelect1">Estado</label>
-                    <select class="form-control" id="listStatus" name="listStatus" required="">
-                      <option value="1">Activo</option>
-                      <option value="2">Inactivo</option>
-                    </select>
-                  </div>
-                  <div class="tile-footer">
-                    <button id="btnActionForm" class="btn btn-primary" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i><span id="btnText">Guardar</span></button>&nbsp;&nbsp;&nbsp;<a class="btn btn-secondary" href="#" data-dismiss="modal"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cancelar</a>
-                  </div>
-              </form>
-            </div>
+          <div class="tile-body">
+            <form id="formRol" name="formRol">
+              <input type="hidden" id="idRol" name="idRol" value="">
+              <div class="form-group">
+                <label class="control-label">Nombre</label>
+                <input class="form-control" id="txtNombre" name="txtNombre" type="text" placeholder="Nombre del Rol" required="">
+              </div>
+              <div class="form-group">
+                <label class="control-label">Descripción</label>
+                <textarea class="form-control" id="txtDescripcion" name="txtDescripcion" rows="2" placeholder="Descripcion del Rol" required=""></textarea>
+              </div>
+              <div class="form-group">
+                <label for="exampleSelect1">Estado</label>
+                <select class="form-control" id="listStatus" name="listStatus" required="">
+                  <option value="1">Activo</option>
+                  <option value="2">Inactivo</option>
+                </select>
+              </div>
+              <div class="tile-footer">
+                <button id="btnActionForm" class="btn btn-primary" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i><span id="btnText">Guardar</span></button>&nbsp;&nbsp;&nbsp;<a class="btn btn-secondary" href="#" data-dismiss="modal"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cancelar</a>
+              </div>
+            </form>
           </div>
+        </div>
       </div>
     </div>
   </div>
@@ -66,44 +66,77 @@
 <div class="modal fade modalPermisos" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-xl">
     <div class="modal-content">
-      
+
       <div class="modal-header">
-          <h5 class="modal-title h4">Permisos Roles de Usuario</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">x</span>
-          </button>
+        <h5 class="modal-title h4">Permisos Roles de Usuario</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">x</span>
+        </button>
       </div>
       <div class="modal-body">
-      <div class="col-md-12">
+        <div class="col-md-12">
           <div class="tile">
-            <h3 class="tile-title">Responsive Table</h3>
-            <div class="table-responsive">
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Módulo</th>
-                    <th>Leer</th>
-                    <th>Escribir</th>
-                    <th>Actualizar</th>
-                    <th>Eliminar</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Usuario</td>
-                    <td><div class="toggle-flip">
-                  <label>
-                    <input type="checkbox"><span class="flip-indecator" data-toggle-on="ON" data-toggle-off="OFF"></span>
-                  </label>
-                </div></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            <form action="" id="formPermisos" name="formPermisos">
+
+              <div class="table-responsive">
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Módulo</th>
+                      <th>Leer</th>
+                      <th>Escribir</th>
+                      <th>Actualizar</th>
+                      <th>Eliminar</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Usuario</td>
+                      <td>
+                        <div class="toggle-flip">
+                          <label>
+                            <input type="checkbox"><span class="flip-indecator" data-toggle-on="ON" data-toggle-off="OFF"></span>
+                          </label>
+                        </div>
+                      </td>
+                      <td>
+                        <div class="toggle-flip">
+                          <label>
+                            <input type="checkbox"><span class="flip-indecator" data-toggle-on="ON" data-toggle-off="OFF"></span>
+                          </label>
+                        </div>
+                      </td>
+                      <td>
+                        <div class="toggle-flip">
+                          <label>
+                            <input type="checkbox"><span class="flip-indecator" data-toggle-on="ON" data-toggle-off="OFF"></span>
+                          </label>
+                        </div>
+                      </td>
+                      <td>
+                        <div class="toggle-flip">
+                          <label>
+                            <input type="checkbox"><span class="flip-indecator" data-toggle-on="ON" data-toggle-off="OFF"></span>
+                          </label>
+                        </div>
+                      </td>
+                      <td>
+                        <div class="toggle-flip">
+                          <label>
+                            <input type="checkbox"><span class="flip-indecator" data-toggle-on="ON" data-toggle-off="OFF"></span>
+                          </label>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              
+              <div class="text-center">
+                <button class="btn btn-success" type="submit"><i class="fa fa-fw fa-lg fa-check-circle" aria-hidden="true"></i>Guardar</button>
+                <button class="btn btn-danger" type="button" data-dismiss="modal"><i class="app-menu_icon fas fa-sign-out-alt" aria-hidden="true"></i> Salir</button>
+              </div>
+            </form> 
           </div>
         </div>
       </div>
