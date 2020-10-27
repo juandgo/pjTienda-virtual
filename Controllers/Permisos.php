5df0597309d4e2d56e85fb0d516c5f2900fd8e50
Grijalba Osorio <juan.grijalba@alcaldiacali.local>
2020-10-27 12:11:24 -0500

    123

--- Controllers/Permisos.php
+++ Controllers/Permisos.php
@@ -11,16 +11,30 @@
 				$arrModulos = $this->model->selectModulos();
 				$arrPermisosRol = $this->model->selectPermisosRol($rolid);
 				$arrPermisos = array('r' => 0, 'w' => 0, 'u' => 0, 'd' => 0);
-				$arrPermisoRol = array('idrol' => $rolid );
+                $arrPermisoRol = array('idrol' => $rolid );
+                //dep sirve para depurar y visualizar en consola los array
+                // dep($arrModulos);
+                // dep($arrPermisosRol);
 
 				if(empty($arrPermisosRol)){
 					for ($i=0; $i < count($arrModulos) ; $i++) { 
 
 						$arrModulos[$i]['permisos'] = $arrPermisos;
                     }
+                }else{
+                    for ($i=0; $i < count($arrModulos); $i++) { 
+                        $arrPermiso =  array('r' => $arrPermisoRol[i]['r'],
+                                             'w' => $arrPermisoRol[i]['w'],
+                                             'u' => $arrPermisoRol[i]['u'],
+                                             'd' => $arrPermisoRol[i]['d']
+                                            ); 
+                        if ($arrModulos[$i]['idmodulo'] == $arrPermisosRol[$i]['moduloid']) {
+                            $arrModulos[$i]['permisos'] =$arrPermisos;
+                        }
+                    }
                 }
+                $arrPermisoRols['modulos'] = $arrModulos;
                 dep($arrModulos);
-                // dep($arrPermisosRol);
 
 
             }
