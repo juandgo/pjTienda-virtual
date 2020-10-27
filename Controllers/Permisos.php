d1593559101e197bd2db5136cf7cb79aa40326d3
Grijalba Osorio <juan.grijalba@alcaldiacali.local>
2020-10-27 13:03:28 -0500

    asdf

--- Controllers/Permisos.php
+++ Controllers/Permisos.php
@@ -1,40 +1,44 @@
 <?php 
     
 	class Permisos extends Controllers{
-		public function __construct(){
+		public function __construct()
+		{
 			parent::__construct();
 		}
 
-		public function getPermisosRol(int $idrol){
+		public function getPermisosRol(int $idrol)
+		{
 			$rolid = intval($idrol);
-			if($rolid > 0){
+			if($rolid > 0)
+			{
 				$arrModulos = $this->model->selectModulos();
 				$arrPermisosRol = $this->model->selectPermisosRol($rolid);
 				$arrPermisos = array('r' => 0, 'w' => 0, 'u' => 0, 'd' => 0);
-                $arrPermisoRol = array('idrol' => $rolid );
-                //dep sirve para depurar y visualizar en consola los array
-                // dep($arrModulos);
-                // dep($arrPermisosRol);
+				$arrPermisoRol = array('idrol' => $rolid );
 
-				if(empty($arrPermisosRol)){
+				if(empty($arrPermisosRol))
+				{
 					for ($i=0; $i < count($arrModulos) ; $i++) { 
 
 						$arrModulos[$i]['permisos'] = $arrPermisos;
-                    }
-                }else{
-                    for ($i=0; $i < count($arrModulos); $i++) { 
-                        $arrPermiso =  array('r' => $arrPermisoRol[i]['r'],
-                                             'w' => $arrPermisoRol[i]['w'],
-                                             'u' => $arrPermisoRol[i]['u'],
-                                             'd' => $arrPermisoRol[i]['d']
-                                            ); 
-                        if ($arrModulos[$i]['idmodulo'] == $arrPermisosRol[$i]['moduloid']) {
-                            $arrModulos[$i]['permisos'] =$arrPermisos;
-                        }
+					}
+				}else{
+					for ($i=0; $i < count($arrModulos); $i++) {
+
+						$arrPermisos = array('r' => $arrPermisosRol[$i]['r'], 
+											 'w' => $arrPermisosRol[$i]['w'], 
+											 'u' => $arrPermisosRol[$i]['u'], 
+											 'd' => $arrPermisosRol[$i]['d'] 
+											);
+						if($arrModulos[$i]['idmodulo'] == $arrPermisosRol[$i]['moduloid'])
+						{
+							$arrModulos[$i]['permisos'] = $arrPermisos;
+						}
                     }
                 }
-                $arrPermisoRols['modulos'] = $arrModulos;
-                dep($arrModulos);
+				$arrPermisoRols['modulos'] = $arrModulos;
+				$html = getModal("modalRoles",$arrPermisoRol);
+                // dep($arrPermisoRol);
 
 
             }
