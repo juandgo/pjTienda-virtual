039767d4f65a1047016299d5fbaa3eb488998d37
Grijalba Osorio <juan.grijalba@alcaldiacali.local>
2020-10-27 09:59:27 -0500

    wqer

--- Models/PermisosModel.php
+++ Models/PermisosModel.php
@@ -1,32 +1,31 @@
 <?php 
+    	class PermisosModel extends Mysql{
+            public $intIdpermiso;
+            public $intRolid;
+            public $intModuloid;
+            public $r;
+            public $w;
+            public $u;
+            public $d;
     
-    class PermisosModel extends Mysql{
-         
-        public $intIdPermiso;
-        public $intRolId;
-        public $intModuloId;
-        public $r;
-        public $w; 
-        public $u;
-        public $d;
-
-        public function __construct(){
-            parent::__construct();
-        }
-
-        public function selectModulos(){
-            $sql = "SELECT * FROM modulo WHERE status != 0";
-            $request = $this->select_all($sql);//ejecuta la instruccion sql
-            return $request;
-        }
-
-        public function selectPermisosRol(int $idrol){
-
-            $this->intRolId = $idrol;
-            $sql = "SELECT * FROM permisos WHERE rolid = $this->intRolId";
-            $request = $this->select_all($sql);//ejecuta la instrucion sql
-            return $request;
-        }
+            public function __construct()
+            {
+                parent::__construct();
+            }
+    
+            public function selectModulos()
+            {
+                $sql = "SELECT * FROM modulo WHERE status != 0";
+                $request = $this->select_all($sql);
+                return $request;
+            }	
+            public function selectPermisosRol(int $idrol)
+            {
+                $this->intRolid = $idrol;
+                $sql = "SELECT * FROM permisos WHERE rolid = $this->intRolid";
+                $request = $this->select_all($sql);
+                return $request;
+            }
     }
 
 ?>
