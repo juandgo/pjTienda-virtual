62ef1974e9933f19674fc607b4d01fcd7d56b662
Grijalba Osorio <juan.grijalba@alcaldiacali.local>
2020-10-27 11:46:43 -0500

    asdfsadf

--- Controllers/Permisos.php
+++ Controllers/Permisos.php
@@ -1,28 +1,25 @@
 <?php 
-    class Permisos extends Controllers{
-        public function __construct(){
-            parent::__construct();//ejecuta el metodo constructor de la clase Controllers
-        }
+    	class Permisos extends Controllers{
+		public function __construct()
+		{
+			parent::__construct();
+		}
 
-        public function getPermisosRol(int $idrol){
-            
-            $rolid = intval($idrol);
-            if ($rolid > 0) {
-                
-                $arrModulos = $this->model->selectModulos();
-                $arrPermisosRol = $this->model->selectPermisosRol($rolid);
-                $arrPermisos = array('r' => 0, 'w' => 0, 'u' => 0, 'd' => 0);
-                $arrPermisosRol = array('idrol' => $rolid );
-                //dep sirve para depurar y visualizar en consola los array
-                // dep($arrModulos);
-                // dep($arrPermisosRol);
-               
+		public function getPermisosRol(int $idrol)
+		{
+			$rolid = intval($idrol);
+			if($rolid > 0)
+			{
+				$arrModulos = $this->model->selectModulos();
+				$arrPermisosRol = $this->model->selectPermisosRol($rolid);
+				$arrPermisos = array('r' => 0, 'w' => 0, 'u' => 0, 'd' => 0);
+				$arrPermisoRol = array('idrol' => $rolid );
 
-                if (empty($arrPermisosRol)) {
-                    
-                    for ($i=0; $i < count($arrModulos) ; $i++) { 
+				if(empty($arrPermisosRol))
+				{
+					for ($i=0; $i < count($arrModulos) ; $i++) { 
 
-                        $arrModulos[$i]['permisos'] = $arrPermisos;
+						$arrModulos[$i]['permisos'] = $arrPermisos;
                     }
                 }
                 dep($arrModulos);
