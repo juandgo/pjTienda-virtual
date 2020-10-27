d246339a59ac867ce251289f101fb53eddb016c7
Grijalba Osorio <juan.grijalba@alcaldiacali.local>
2020-10-27 11:48:44 -0500

    as

--- Controllers/Permisos.php
+++ Controllers/Permisos.php
@@ -1,22 +1,19 @@
 <?php 
-    	class Permisos extends Controllers{
-		public function __construct()
-		{
+    
+	class Permisos extends Controllers{
+		public function __construct(){
 			parent::__construct();
 		}
 
-		public function getPermisosRol(int $idrol)
-		{
+		public function getPermisosRol(int $idrol){
 			$rolid = intval($idrol);
-			if($rolid > 0)
-			{
+			if($rolid > 0){
 				$arrModulos = $this->model->selectModulos();
 				$arrPermisosRol = $this->model->selectPermisosRol($rolid);
 				$arrPermisos = array('r' => 0, 'w' => 0, 'u' => 0, 'd' => 0);
 				$arrPermisoRol = array('idrol' => $rolid );
 
-				if(empty($arrPermisosRol))
-				{
+				if(empty($arrPermisosRol)){
 					for ($i=0; $i < count($arrModulos) ; $i++) { 
 
 						$arrModulos[$i]['permisos'] = $arrPermisos;
