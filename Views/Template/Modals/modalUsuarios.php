26588f71ad5495779764c03c64f4c5fae92e7da7
Grijalba Osorio <juan.grijalba@alcaldiacali.local>
2020-10-28 17:04:58 -0500

    Haciendo vista del mudulo usuarios

---
+++ Views/Template/Modals/modalUsuarios.php
@@ -0,0 +1,34 @@
+<!-- modalRoles -->
+<div class="modal fade" id="modalFormUsuario" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header headerRegister">
+        <h5 class="modal-title" id="titleModal">Nuevo Usuario</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+    
+            <form id="formUsuario" name="formUsuario" class="formHorizontal">
+              <input type="hidden" id="idUsuario" name="idUsuario" value="">
+              <p class="text-primary">Todos los campos son obligatorios.</p>
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="textIdentificacion">Identificación</label>s
+                    <input type="text" class="form-control" id="textIdentificacion" name="textIdentificacion" required="">
+                </div>
+              </div>
+              
+              <div class="tile-footer">
+                <button id="btnActionForm" class="btn btn-primary" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i><span id="btnText">Guardar</span></button>&nbsp;&nbsp;&nbsp;<a class="btn btn-secondary" href="#" data-dismiss="modal"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cancelar</a>
+              </div>
+            </form>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
