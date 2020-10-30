6f1a02e35d604d90b37ca310352a7ebacda2f18f
Grijalba Osorio <juan.grijalba@alcaldiacali.local>
2020-10-30 13:22:20 -0500

    asdf

--- Controllers/Usuarios.php
+++ Controllers/Usuarios.php
@@ -14,9 +14,19 @@
 
         public function setUsuario(){
             if ($_POST) {
-                dep($_POST);
-
-            echo $_POST['listRolid'];
+                //Valida si no existe algun dato en el elemento//Nota: esta validacion ya se hizo en js pero tambien es importante hacerlo aca dellado del backend
+                if (empty($_POST['txtIdentificacion']) || empty($_POST['txtNombre']) || empty($_POST['txtApellido']) || empty($_POST['txtTelefono']) || empty($_POST['txtEmail']) || empty($_POST['listStatus'])) {
+                    $arrRespose = array("status" => false, "msg" =>'Datos incorrectos. ');
+                }else{
+                    $strIdentificacion = intval($_POST['txtIdentificacion']);
+                    $strNombre = strclean($_POST['txtNombre']);
+                    $strApellido = strClean($_POST['txtApellido']);
+                    $intTelefono= intval($_POST['txtTelefono']);
+                    $strEmail = strClean($_POST['txtEmail']);
+                    $intTipoUsuario = intval($_POST['listRolid']);
+                    $strPassword = intval($_POST['listStatuss']); 
+                    
+                }
             }
             die();
 
