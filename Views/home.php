9145dcc3b74860846aea8bb5808d79c0db7fe47a
Grijalba Osorio <juan.grijalba@alcaldiacali.local>
2020-11-12 16:21:45 -0500

    lkjhg

--- Views/home.php
+++ Views/home.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Mini Framework - AbelOSh</title>
+  <title>Tienda Virtual - JuanDa97</title>
 </head>
 <body>
     <p>Mini Framework V1.0 - <a href="https://JuanDa97.com">www.jd97.com</a></p>
