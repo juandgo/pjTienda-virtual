cc6cf26692da6712dbbfe5bc95e45f043f4970f8
JuanDa97 <juandavidgo1997@gmail.com>
2021-06-11 17:24:51 -0500

    stilos de imagnes de productos

--- Views/Template/Modals/modalProductos.php
+++ Views/Template/Modals/modalProductos.php
@@ -69,9 +69,50 @@
                             </div>
                         </div>
                      </div>
-                    </div>
+                  </div>
                 <div class="tile-footer">
-                 
+                  <div class="form-group col-md-12">
+                    <div id="containerGallery">
+                      <span>Galería de fotos</span>
+                      <button class="btnAddImage btn btn-info btn-sm" type="button">
+                        <i class="fas fa-plus"></i>
+                      </button>
+                    </div>
+                    <hr>
+                    <div id="containerImages">
+                      <div id="div24">
+                        <div class="prevImage">
+                          <img src="<?= media(); ?>/images/uploads/tesla.jpg">
+                        </div>
+                        <input type="file" name="foto" id="img1" class="inputUploadfile">
+                        <label for="img1" class="btnUploadfile"><i class="fas fa-upload"></i></label>
+                        <button class="btnDeleteImage" type="button" onclick="fntDelItem('div24');">
+                          <i class="fas fa-trash-alt"></i>
+                        </button>
+                      </div>
+                      <div id="div24">
+                        <div class="prevImage">
+                          <img class="loading" src="<?= media(); ?>/images/loading.svg">
+                        </div>
+                        <input type="file" name="foto" id="img1" class="inputUploadfile">
+                        <label for="img1" class="btnUploadfile"><i class="fas fa-upload"></i></label>
+                        <button class="btnDeleteImage" type="button" onclick="fntDelItem('div24');">
+                          <i class="fas fa-trash-alt"></i>
+                        </button>
+                      </div>
+                      <div id="div24">
+                        <div class="prevImage">
+                          <img src="<?= media(); ?>/images/uploads/tesla.jpg">
+                        </div>
+                        <input type="file" name="foto" id="img1" class="inputUploadfile">
+                        <label for="img1" class="btnUploadfile"><i class="fas fa-upload"></i></label>
+                        <button class="btnDeleteImage" type="button" onclick="fntDelItem('div24');">
+                          <i class="fas fa-trash-alt"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+
                 </div>
             </form>
       </div>
