677059b9d57dde652fd0ceb3930a8a3af11d11c7
JuanDa97 <juandavidgo1997@gmail.com>
2021-07-16 16:37:30 -0500

    productos categorias

--- Views/Tienda/categoria.php
+++ Views/Tienda/categoria.php
@@ -216,14 +216,19 @@
 			<div class="row isotope-grid">
 			<?php 
 			for ($p=0; $p < count($arrProductos) ; $p++) { 
+				if(count($arrProductos[$p]['images']) > 0){
+					$portada = $arrProductos[$p]['images'][0]['url_image'];
+				}else{
+					$portada = media().'/images/uploads/product.png';//Imagen por defecto 
+				}
 			?>	
 				<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
 					<!-- Block2 -->
 					<div class="block2">
 						<div class="block2-pic hov-img0">
-							<img src="<?= media(); ?>/tienda/images/product-01.jpg" alt="<?= $arrProductos[$p]['nombre']?>">
+							<img src="<?= $portada ?>" alt="<?= $arrProductos[$p]['nombre']?>">
 
-							<a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+							<a href="<?= base_url().'/tienda/producto/'.$arrProductos[$p]['nombre']; ?>" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04">
 								Ver producto
 							</a>
 						</div>
@@ -240,18 +245,19 @@
 							</div>
 
 							<div class="block2-txt-child2 flex-r p-t-3">
-								<a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
-									<img class="icon-heart1 dis-block trans-04" src="<?= media(); ?>/images/icons/icon-heart-01.png" alt="ICON">
-									<img class="icon-heart2 dis-block trans-04 ab-t-l" src="<?= media(); ?>/images/icons/icon-heart-02.png" alt="ICON">
+								<a href="<?= base_url().'/tienda/producto/'.$arrProductos[$p]['nombre']; ?>" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+									<img class="icon-heart1 dis-block trans-04" src="<?= media() ?>/tienda/images/icons/icon-heart-01.png" alt="ICON">
+									<img class="icon-heart2 dis-block trans-04 ab-t-l" src="<?= media() ?>/tienda/images/icons/icon-heart-02.png" alt="ICON">
 								</a>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
 			<?php
 			}
 			?>
+			</div>
+			
 			<!-- Load more -->
 			<div class="flex-c-m flex-w w-full p-t-45">
 				<a href="#" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
